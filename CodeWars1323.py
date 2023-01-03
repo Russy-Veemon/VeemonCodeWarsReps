c9cd69bea032ee71d664d9648590ff20e71f2da5
Russell Valencia <russyvsc@gmail.com>
2023-01-03 13:44:50 -0800

    1st algo of the day

---
+++ CodeWars1323.py
@@ -0,0 +1,20 @@
+# ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+# If the function is passed a valid PIN string, return true, else return false.
+
+# Examples (Input --> Output)
+# "1234"   -->  true
+# "12345"  -->  false
+# "a234"   -->  false
+
+def validate_pin(pin):
+    if pin.isdigit() == True:
+        for x in pin:
+            if len(pin) == 4:
+                return True
+            elif len(pin) == 6:
+                return True
+            else:
+                return False
+    else:
+        return False
