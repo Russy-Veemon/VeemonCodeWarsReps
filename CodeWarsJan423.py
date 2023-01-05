1bfec66ca9da9e1d8c5fc74b15a73d17ad6cb0a6
Russell Valencia <russyvsc@gmail.com>
2023-01-04 20:02:34 -0800

    3rd algo of the day

--- CodeWarsJan423.py
+++ CodeWarsJan423.py
@@ -37,3 +37,11 @@ def rental_car_cost(d):
         rent += 40*d
     return rent
 
+# You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+# Complete the method which accepts such an array, and returns that single different number.
+# The input array will always be valid! (odd-length >= 3)
+
+def stray(arr):
+    for x in arr:
+        if arr.count(x) == 1:
+            return x
