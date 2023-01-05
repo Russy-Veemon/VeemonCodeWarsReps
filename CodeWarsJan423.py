f114aba8717bf644dd3a61a35c9a2220bf1c7cb8
Russell Valencia <russyvsc@gmail.com>
2023-01-04 19:41:13 -0800

    2nd algo of the day edit

--- CodeWarsJan423.py
+++ CodeWarsJan423.py
@@ -1,14 +1,9 @@
 # <!-- As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
-
 # The input to the function will be an array of three distinct numbers (Haskell: a tuple).
-
 # For example:
-
 # gimme([2, 3, 1]) => 0
 # 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
-
 # Another example (just to make sure it is clear):
-
 # gimme([5, 10, 14]) => 1
 # 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1. -->
 
@@ -25,3 +20,20 @@ def gimme(input):
         return 2
     if input[2] > input[0] and input [2] < input[1]:
         return 2
+
+
+# After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+# You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+# Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+# Write a code that gives out the total amount for different days(d).
+
+def rental_car_cost(d):
+    rent = 0
+    if d>= 7:
+        rent += (40*d) - 50
+    elif d >= 3:
+        rent += (40*d) - 20
+    else:
+        rent += 40*d
+    return rent
+
