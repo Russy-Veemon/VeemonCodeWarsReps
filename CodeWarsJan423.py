22a8b0bda8dbbd5bae1caa4e9a749d650b18b495
Russell Valencia <russyvsc@gmail.com>
2023-01-04 20:09:27 -0800

    5th algo of the day

--- CodeWarsJan423.py
+++ CodeWarsJan423.py
@@ -49,3 +49,15 @@ def stray(arr):
 # Now you have to write a function that takes an argument and returns the square of it.
 def square(n):
     return n**2
+
+# Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+# For example (Input -> Output):
+# 2 -> 3 (1 + 2)
+# 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+def summation(num):
+    summation = num
+    while num > 0:
+        num = num - 1
+        summation += num
+    return summation
