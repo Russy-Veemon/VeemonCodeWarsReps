e960b6403b8ec2133b3c5d84a9fd70c77c89f23a
Russell Valencia <russyvsc@gmail.com>
2023-01-07 16:09:52 -0800

    3rd algo of the day

--- CodeWarsJan723.py
+++ CodeWarsJan723.py
@@ -22,4 +22,15 @@ def find_multiples(integer, limit):
         array.append(add)
         add += integer
     return array
-        
+    
+# Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+# Examples
+# "www.codewars.com#about" --> "www.codewars.com"
+# "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+def remove_url_anchor(url):
+    final_char = url.find("#")
+    if final_char == -1:
+        return url
+    return url[:final_char]
+
