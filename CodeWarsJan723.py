5bb417cae10053c5fdabae44701ae7774266ec7d
Russell Valencia <russyvsc@gmail.com>
2023-01-07 17:07:42 -0800

    4th algo of the day

--- CodeWarsJan723.py
+++ CodeWarsJan723.py
@@ -34,3 +34,14 @@ def remove_url_anchor(url):
         return url
     return url[:final_char]
 
+# Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+# The output should be two capital letters with a dot separating them.
+# It should look like this:
+# Sam Harris => S.H
+# patrick feeney => P.F
+
+def abbrev_name(name):
+    first_name, last_name = name.split()
+    first_initial = first_name[0].upper()
+    last_initial = last_name[0].upper()
+    return f"{first_initial}.{last_initial}"
