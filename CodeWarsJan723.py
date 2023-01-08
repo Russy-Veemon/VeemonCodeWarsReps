a95cbb998f7a70427dad128cc1f5b8a7ab7fe55d
Russell Valencia <russyvsc@gmail.com>
2023-01-07 17:35:13 -0800

    5th algo of the day

--- CodeWarsJan723.py
+++ CodeWarsJan723.py
@@ -45,3 +45,14 @@ def abbrev_name(name):
     first_initial = first_name[0].upper()
     last_initial = last_name[0].upper()
     return f"{first_initial}.{last_initial}"
+
+# Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+# Examples:
+# solution('abc', 'bc') # returns true
+# solution('abc', 'd') # returns false
+
+def solution(text, ending):
+    if text[-len(ending):] == ending:
+        return True
+    else:
+        return False
