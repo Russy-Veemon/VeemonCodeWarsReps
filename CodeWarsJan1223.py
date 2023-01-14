a1f730d008c1c55e84e6f20a14ba64c0f0984cb4
Russell Valencia <russyvsc@gmail.com>
2023-01-13 23:35:21 -0800

    4th algo filtering strings with duplicate characters, by comparing the len of string vs the len of a set string which has no dups

--- CodeWarsJan1223.py
+++ CodeWarsJan1223.py
@@ -33,3 +33,11 @@ def duplicate_encode(word):
         else:
             new_string += '('
     return new_string
+
+# An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+# Example: (Input --> Output)
+# "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+def is_isogram(string):
+    string = string.lower()
+    return len(set(string)) == len(string)
