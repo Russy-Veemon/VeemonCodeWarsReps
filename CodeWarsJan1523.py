618f1ef6845050d92e86274ccaa438b737a1aa39
Russell Valencia <russyvsc@gmail.com>
2023-01-15 23:07:53 -0800

    2nd algo of the night, counting timers between customer checkouts

--- CodeWarsJan1523.py
+++ CodeWarsJan1523.py
@@ -27,3 +27,23 @@ def greet(language):
         return languages[language]
     else:
         return languages["english"]
+
+# There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+# input
+# customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+# n: a positive integer, the number of checkout tills.
+# output
+# The function should return an integer, the total time required.
+
+def queue_time(customers, n):
+    # Initialize time and till counters
+    time = 0
+    tills = [0] * n
+    while customers:
+        # Find the next available till
+        next_till = tills.index(min(tills))
+        # Assign the next customer to the till
+        tills[next_till] += customers.pop(0)
+        # Update the time to the maximum time of all tills
+        time = max(tills)
+    return time
