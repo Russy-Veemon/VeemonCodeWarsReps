8af81e4c4ab6dc8bca29f3d7c55a03097820b992
Russell Valencia <russyvsc@gmail.com>
2023-01-16 21:16:56 -0800

    2nd algo of the night, return int of amt ltrs drank over cycle

--- CodeWarsJan1623.py
+++ CodeWarsJan1623.py
@@ -25,3 +25,15 @@ def how_much_i_love_you(nb_petals):
         return "madly"
     elif petals == 6:
         return "not at all"
+
+# Nathan loves cycling.
+# Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+# You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+# For example:
+# time = 3 ----> litres = 1
+# time = 6.7---> litres = 3
+# time = 11.8--> litres = 5
+
+def litres(time):
+    water_drank = time * 0.5
+    return int(water_drank)
