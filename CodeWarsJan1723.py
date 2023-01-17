3eda39c2f05d0e1858a0d89a13b6fd54547a8599
Russell Valencia <russyvsc@gmail.com>
2023-01-17 14:45:49 -0800

    creating a function to calculate BMI and return its proper category

--- CodeWarsJan1723.py
+++ CodeWarsJan1723.py
@@ -43,3 +43,20 @@ def between(a,b):
 
 def remove_exclamation_marks(s):
     return s.replace('!', "")
+
+# Write function bmi that calculates body mass index (bmi = weight / height2).
+# if bmi <= 18.5 return "Underweight"
+# if bmi <= 25.0 return "Normal"
+# if bmi <= 30.0 return "Overweight"
+# if bmi > 30 return "Obese"
+
+def bmi(weight, height):
+    bmi = weight / (height**2)
+    if bmi <= 18.5: 
+        return "Underweight"
+    if bmi <= 25.0 :
+        return "Normal"
+    if bmi <= 30.0 :
+        return "Overweight"
+    if bmi > 30 :
+        return "Obese"
