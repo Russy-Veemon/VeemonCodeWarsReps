a6b9ba6ec07b940917230c68d26647e563858e21
Russell Valencia <russyvsc@gmail.com>
2023-01-17 23:04:47 -0800

    reversing every index in a string, while keeping each word in a string in the same spot

--- CodeWarsJan1723.py
+++ CodeWarsJan1723.py
@@ -68,3 +68,13 @@ def bmi(weight, height):
 
 def find_difference(a, b):
     return  abs((a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2]))
+
+# Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+# Examples
+# "This is an example!" ==> "sihT si na !elpmaxe"
+# "double  spaces"      ==> "elbuod  secaps"
+
+def reverse_words(text):
+    words = text.split(" ")
+    reversed_words = [word[::-1] for word in words]
+    return " ".join(reversed_words)
