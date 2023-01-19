b5fcca3a12c54a1dcafe134109cda7c6b5f73aae
Russell Valencia <russyvsc@gmail.com>
2023-01-18 21:40:35 -0800

    using min(array) and max(array) to find the largest and smallest value in an array and return it

--- CdoeWarsJan1823.py
+++ CdoeWarsJan1823.py
@@ -7,3 +7,11 @@ def find_needle(haystack):
     for x in haystack:
         if x == "needle":
             return "found the needle at position " + str(haystack.index(x))
+
+# Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)
