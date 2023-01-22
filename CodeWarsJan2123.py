170f270aafa7af93914ae9f3d2e92596891011bb
Russell Valencia <russyvsc@gmail.com>
2023-01-21 22:47:19 -0800

    function that searches for variable in givin array

--- CodeWarsJan2123.py
+++ CodeWarsJan2123.py
@@ -45,3 +45,13 @@ def comp(a, b):
         a_squared.sort()
         b.sort()
         return a_squared == b
+
+# You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+# Array can contain numbers or strings. X can be either.
+# Return true if the array contains the value, false if not.
+
+def check(array, thing):
+    if thing in array:
+        return True
+    else:
+        return False
