83c7dce305876273627243842950da12b7704404
Russell Valencia <russyvsc@gmail.com>
2023-01-21 22:38:03 -0800

    comparing two arrays, if both are the same length and contain indexes, a should be the sqr root of b if not return false

--- CodeWarsJan2123.py
+++ CodeWarsJan2123.py
@@ -32,3 +32,16 @@ def solution(s):
 
 def reverse_list(l):
     return l[::-1]
+
+# Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+def comp(a, b):
+    if a is None or b is None:
+        return False
+    elif len(a) != len(b):
+        return False
+    else:
+        a_squared = [i ** 2 for i in a]
+        a_squared.sort()
+        b.sort()
+        return a_squared == b
