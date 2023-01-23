941389ce2658fa34e653627edf82ac61bc6e8241
Russell Valencia <russyvsc@gmail.com>
2023-01-23 11:57:39 -0800

    a function that repeats a string by the number given and returns it

--- CodeWarsJan2323.py
+++ CodeWarsJan2323.py
@@ -70,3 +70,11 @@ def is_valid_walk(walk):
 
 def xor(a,b):
     return a ^ b
+
+# Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+# Examples (input -> output)
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+def repeat_str(repeat, string):
+    return repeat * string
