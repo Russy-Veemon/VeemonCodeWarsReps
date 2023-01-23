94a43f352cfff7a0f5fa517ca58326122ae67162
Russell Valencia <russyvsc@gmail.com>
2023-01-23 12:05:55 -0800

    function that returns a string with multiple spaces in reverse

--- CodeWarsJan2323.py
+++ CodeWarsJan2323.py
@@ -78,3 +78,15 @@ def xor(a,b):
 
 def repeat_str(repeat, string):
     return repeat * string
+
+# You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+# As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+# Example (Input --> Output)
+# "Hello World" --> "World Hello"
+# "Hi There." --> "There. Hi"
+# Happy coding!
+
+def reverse(st):
+    words = st.split()
+    backward = words[::-1]
+    return ' '.join([x for x in backward])
