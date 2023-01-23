89672f8885991dfbf6bfc16626fbdae5e8773129
Russell Valencia <russyvsc@gmail.com>
2023-01-23 01:58:20 -0800

    using .sort and .join(list()) to iterate through a list grab the first index, and add things between each character of that string of the first index

--- CodeWarsJan2323.py
+++ CodeWarsJan2323.py
@@ -39,3 +39,12 @@ def positive_sum(arr):
         if x > 0:
             total += x
     return total
+
+# You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+# The returned value must be a string, and have "***" between each of its letters.
+# You should not remove or add elements from/to the array.
+
+def two_sort(array):
+    array.sort()
+    answer = array[0]
+    return '***'.join(list(answer))
