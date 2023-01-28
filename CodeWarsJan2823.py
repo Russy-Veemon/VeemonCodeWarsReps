1de37fd0b9e247cdb35373795919c00b3edcd4ed
Russell Valencia <russyvsc@gmail.com>
2023-01-28 11:32:13 -0800

    function that returns a string that has been parted into chunks, and reverses those chunks

--- CodeWarsJan2823.py
+++ CodeWarsJan2823.py
@@ -9,3 +9,24 @@ def solution(nums):
     else:
         nums.sort()
         return nums
+
+# The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+# If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+# If
+# sz is <= 0 or if str is empty return ""
+# sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+
+def rev_rot(strng, sz):
+    if sz <= 0 or not strng or sz > len(strng):
+        return ""
+    chunks = [strng[i:i+sz] for i in range(0, len(strng), sz)]
+    result = ""
+    for chunk in chunks:
+        if len(chunk) < sz:
+            break
+        sum_of_cubes = sum([int(c)**3 for c in chunk])
+        if sum_of_cubes % 2 == 0:
+            result += chunk[::-1]
+        else:
+            result += chunk[1:] + chunk [0]
+    return result
