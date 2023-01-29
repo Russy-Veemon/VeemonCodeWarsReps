e3265ced6213552a5c57618a83200d21356662c5
Russell Valencia <russyvsc@gmail.com>
2023-01-28 23:39:21 -0800

    returning the sum of the array

--- CodeWarsJan2823.py
+++ CodeWarsJan2823.py
@@ -58,3 +58,22 @@ def check_coupon(entered_code, correct_code, current_date, expiration_date):
     if curr_date > exp_date:
         return False
     return True
+
+# Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+# Examples
+# Input: [1, 5.2, 4, 0, -1]
+# Output: 9.2
+# Input: []
+# Output: 0
+# Input: [-2.398]
+# Output: -2.398
+# Assumptions
+# You can assume that you are only given numbers.
+# You cannot assume the size of the array.
+# You can assume that you do get an array and if the array is empty, return 0.
+
+def sum_array(a):
+    if a is None:
+        return []
+    else:
+        return sum(a)
