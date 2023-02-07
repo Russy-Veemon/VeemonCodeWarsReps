4a3a5fd5b37ef53bcf7fcf15a2de845ed7d4ab52
Russell Valencia <russyvsc@gmail.com>
2023-02-06 16:57:03 -0800

    using a dictionary to compare given direction and remove duplicate directions that are cancelled out by their opposite in the new list

--- CodeWarsFeb623.py
+++ CodeWarsFeb623.py
@@ -11,3 +11,19 @@ def alphabet_position(text):
         if char in alphabet:
             result += str(ord(char) - 96) + ' '
     return result.strip()
+
+# Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+# The Haskell version takes a list of directions with data Direction = North | East | West | South.
+# The Clojure version returns nil when the path is reduced to nothing.
+# The Rust version takes a slice of enum Direction {North, East, West, South}.
+# See more examples in "Sample Tests:"
+
+def dirReduc(arr):
+    opposite = {'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'}
+    new_dir = []
+    for direction in arr:
+        if new_dir and new_dir[-1] == opposite[direction]:
+            new_dir.pop()
+        else:
+            new_dir.append(direction)
+    return new_dir
