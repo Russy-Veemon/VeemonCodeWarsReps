8db27febb48ba3aa4570e0158a79ce58754550a3
Russell Valencia <russyvsc@gmail.com>
2023-02-06 23:26:57 -0800

    function that checks if current array contains anything from geese and removes it

--- CodeWarsFeb623.py
+++ CodeWarsFeb623.py
@@ -39,3 +39,20 @@ def high(x):
     words = x.split()
     scores = [sum(ord(c) - 96 for c in word) for word in words]
     return words[scores.index(max(scores))]
+
+# Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+# The geese are any strings in the following array, which is pre-populated in your solution:
+#   ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+# For example, if this array were passed as an argument:
+#  ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+# Your function would return the following array:
+# ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+# The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+
+geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+def goose_filter(birds):
+    new_birds = []
+    for x in birds:
+        if x not in geese:
+            new_birds.append(x)
+    return new_birds
