ad0533aec48282ceb70bd41dc11b7f78ed9fbf9d
Russell Valencia <russyvsc@gmail.com>
2023-02-06 17:16:45 -0800

    using ord to calculate the best scoring words and returning that word

--- CodeWarsFeb623.py
+++ CodeWarsFeb623.py
@@ -27,3 +27,15 @@ def dirReduc(arr):
         else:
             new_dir.append(direction)
     return new_dir
+
+# Given a string of words, you need to find the highest scoring word.
+# Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+# For example, the score of abad is 8 (1 + 2 + 1 + 4).
+# You need to return the highest scoring word as a string.
+# If two words score the same, return the word that appears earliest in the original string.
+# All letters will be lowercase and all inputs will be valid.
+
+def high(x):
+    words = x.split()
+    scores = [sum(ord(c) - 96 for c in word) for word in words]
+    return words[scores.index(max(scores))]
