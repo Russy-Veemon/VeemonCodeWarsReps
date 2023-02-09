edbf32e50e5d7bfd705aca1c21c6cca5dff89e97
Russell Valencia <russyvsc@gmail.com>
2023-02-08 22:11:45 -0800

    function that returns key value pairs if found, if key not found return beer

--- CodeWarsFeb723.py
+++ CodeWarsFeb723.py
@@ -27,3 +27,26 @@ def include(arr,item):
 
 def better_than_average(class_points, your_points):
     return your_points > sum(class_points) / len(class_points)
+
+# Complete the function that receives as input a string, and produces outputs according to the following table:
+# Input	Output
+# "Jabroni"	"Patron Tequila"
+# "School Counselor"	"Anything with Alcohol"
+# "Programmer"	"Hipster Craft Beer"
+# "Bike Gang Member"	"Moonshine"
+# "Politician"	"Your tax dollars"
+# "Rapper"	"Cristal"
+# anything else	"Beer"
+# Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+# Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+def get_drink_by_profession(param):
+    professions = {
+        "Jabroni": "Patron Tequila",
+        "School Counselor": "Anything with Alcohol",
+        "Programmer": "Hipster Craft Beer",
+        "Bike Gang Member": "Moonshine",
+        "Politician": "Your tax dollars",
+        "Rapper": "Cristal"
+    }
+    return professions.get(param.capitalize(), "Beer")
