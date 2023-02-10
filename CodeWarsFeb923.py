3789a55d60e738dac340f3844a7b70376352b5d5
Russell Valencia <russyvsc@gmail.com>
2023-02-09 21:45:36 -0800

    function that removes all duplicates from an array and return the array in the same order

--- CodeWarsFeb923.py
+++ CodeWarsFeb923.py
@@ -5,3 +5,14 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+    
+
+# Define a function that removes duplicates from an array of numbers and returns it as a result.
+# The order of the sequence has to stay the same.
+
+def distinct(seq):
+    new_seq = []
+    for thing in seq:
+        if thing not in new_seq:
+            new_seq.append(thing)
+    return new_seq
