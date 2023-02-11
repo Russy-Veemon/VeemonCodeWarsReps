6b13e920d2404e438e0a722bcd79633a7112bbe7
Russell Valencia <russyvsc@gmail.com>
2023-02-11 09:23:39 -0800

    function that return the difference between the number of characters in the two largest strings in two separate arrays

--- CodeWarsFeb1123.py
+++ CodeWarsFeb1123.py
@@ -8,3 +8,18 @@ def remainder(a,b):
         return a % b
     else: 
         return b % a
+    
+# You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+# Find max(abs(length(x) − length(y)))
+# If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+def mxdiflg(a1, a2):
+    if a1 == []  or a2 == []:
+        return -1
+    else:
+        max_diff = -1
+        for x in a1:
+            for y in a2:
+                diff = abs(len(x) - len(y))
+                max_diff = max(max_diff, diff)
+        return max_diff
