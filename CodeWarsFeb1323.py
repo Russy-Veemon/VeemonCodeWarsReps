b6b110efd2ec9a7fccc875d8a93a3586c17f47d8
Russell Valencia <russyvsc@gmail.com>
2023-02-13 21:18:48 -0800

    function that returns the number of lightsabers someone owns

--- CodeWarsFeb1323.py
+++ CodeWarsFeb1323.py
@@ -74,3 +74,16 @@ def row_weights(array):
         else:
             team2 += weight
     return (team1, team2)
+
+
+# Inspired by the development team at Vooza, write the function that
+# accepts the name of a programmer, and
+# returns the number of lightsabers owned by that person.
+# The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+# Note: your function should have a default parameter.
+
+def how_many_light_sabers_do_you_own(name=' '):
+    if name == "Zach":
+        return 18
+    else:
+        return 0
