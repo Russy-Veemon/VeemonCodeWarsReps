4a4d5d81c8362fe95badd39f5c4e9adf5773871a
Russell Valencia <russyvsc@gmail.com>
2023-02-20 09:56:06 -0800

    function that returns parts of an array equal to the position that n provides

--- CodeWarsFeb2023.py
+++ CodeWarsFeb2023.py
@@ -30,3 +30,10 @@ def sum_of_minimums(numbers):
     for x in numbers:
         min_vals.append(min(x))
     return sum(min_vals)
+
+# Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+# If you need help, here's a reference:
+# https://docs.python.org/3/library/stdtypes.html#sequence-types-list-tuple-range
+
+def take(arr,n):
+    return arr[:n]
