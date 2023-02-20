f81638b0150149b1117268c576ebb36c7e4c4b7c
Russell Valencia <russyvsc@gmail.com>
2023-02-20 15:20:38 -0800

    function that returns the conversion from USD to Chinese Yuan

--- CodeWarsFeb2023.py
+++ CodeWarsFeb2023.py
@@ -75,3 +75,12 @@ def cockroach_speed(s):
 
 def sp_eng(sentence): 
     return 'english' in sentence.lower()
+
+# Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+# Examples (Input -> Output)
+# 15  -> '101.25 Chinese Yuan'
+# 465 -> '3138.75 Chinese Yuan'
+# The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
+
+def usdcny(usd):
+    return str((usd)*6.75) + " Chinese Yuan"
