3b42cee17477167ff2e6e455a3c4abdc2dc47937
Russell Valencia <russyvsc@gmail.com>
2023-02-20 14:53:05 -0800

    function that returns the amount of days before the threshhold is met

--- CodeWarsFeb2023.py
+++ CodeWarsFeb2023.py
@@ -50,3 +50,15 @@ def count_sheep(n):
         for i in range(1, n + 1):
             count += f"{i} sheep..."
     return count
+
+# This program tests the life of an evaporator containing a gas.
+# We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+# The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+def evaporator(content, evap_per_day, threshold):
+    n = 0
+    threshold_ml = (threshold / 100) * content
+    while content > threshold_ml:
+        n += 1
+        content *= (1 - (evap_per_day / 100))
+    return n
