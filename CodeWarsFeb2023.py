10057cbe3d08aeb758285fa8d2c39455c4cb4752
Russell Valencia <russyvsc@gmail.com>
2023-02-20 14:42:59 -0800

    function that counts sheep number that was inputted

--- CodeWarsFeb2023.py
+++ CodeWarsFeb2023.py
@@ -37,3 +37,16 @@ def sum_of_minimums(numbers):
 
 def take(arr,n):
     return arr[:n]
+
+# If you can't sleep, just count sheep!!
+# Task:
+# Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+def count_sheep(n):
+    count = ''
+    if n == 0:
+        return ''
+    elif n > 0:
+        for i in range(1, n + 1):
+            count += f"{i} sheep..."
+    return count
