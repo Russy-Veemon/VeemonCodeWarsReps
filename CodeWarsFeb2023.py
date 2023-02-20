7546df20293ad262b6d53a7035588a6b1d0c37de
Russell Valencia <russyvsc@gmail.com>
2023-02-20 09:47:37 -0800

    function that returns an array of min values from nested arrays inputted

--- CodeWarsFeb2023.py
+++ CodeWarsFeb2023.py
@@ -14,3 +14,19 @@ def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
     for x in all_ages:
         new_arr.append(x*x)
     return int((math.sqrt(sum(new_arr)))/2)
+
+# Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+# For Example:
+# [ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+# , [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+# , [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+# ]
+# So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+# Note: You will always be given a non-empty list containing positive values.
+# ENJOYCODING :)
+
+def sum_of_minimums(numbers):
+    min_vals = []
+    for x in numbers:
+        min_vals.append(min(x))
+    return sum(min_vals)
