1d03e67734cf3c651433c60328c86e85ed9fcc4c
Russell Valencia <russyvsc@gmail.com>
2023-02-26 20:59:14 -0800

    function now annotated

--- CodeWarsScreenshots/Feb2023/CodeWarsFeb2623.py
+++ CodeWarsScreenshots/Feb2023/CodeWarsFeb2623.py
@@ -8,4 +8,6 @@ import re
 
 def validate_usr(username):
     regex = r"^[a-z0-9_]{4,16}$"
+    # Define the expression pattern as a raw string
     return bool(re.match(regex, username))
+    # use re.match to check if the username matches the pattern above
