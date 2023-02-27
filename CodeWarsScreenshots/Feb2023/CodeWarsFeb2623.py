163433b63da8ae2573bfbbce8185df0bb95f0347
Russell Valencia <russyvsc@gmail.com>
2023-02-26 21:04:41 -0800

    function that finds the largest number in an array and returns it even if there are dups

--- CodeWarsScreenshots/Feb2023/CodeWarsFeb2623.py
+++ CodeWarsScreenshots/Feb2023/CodeWarsFeb2623.py
@@ -11,3 +11,22 @@ def validate_usr(username):
     # Define the expression pattern as a raw string
     return bool(re.match(regex, username))
     # use re.match to check if the username matches the pattern above
+
+
+# Find the number with the most digits.
+# If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+def find_longest(arr):
+    max_digits = 0
+    max_digits_num = None
+    
+    for num in arr:
+        # Count the number of digits in the current number
+        num_digits = len(str(num))
+        
+        # If the current number has more digits than the previous max, update the max
+        if num_digits > max_digits:
+            max_digits = num_digits
+            max_digits_num = num
+            
+    return max_digits_num
